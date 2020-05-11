45f13e7c6292da904a541742cc79d4ef6333b47d
H.Z <he.zhu#sindrax.cn>
2020-05-11 17:55:18 +0800

    链表反转

--- src/views/LinkedList/index.tsx
+++ src/views/LinkedList/index.tsx
@@ -5,29 +5,18 @@ export interface LinkedListProps {
   
 }
 
-// const ARR = [5, 3, 10, 4, 6, 9, 17, 20, 15, 19,16, 2, 1]
+const ARR = [5, 3, 10, 4]
  
 const LinkedListPage: React.SFC<LinkedListProps> = () => {
   const initList = () => {
     const listed = new LinkedList()
 
-    // for (let i = 0; i<ARR.length;i++) {
-    //   const item = LinkedList.createNode(ARR[i])
-    //   listed.insert(item)
-    // }
+    for (let i = 0; i<ARR.length;i++) {
+      const item = LinkedList.createNode(ARR[i])
+      listed.insert(item)
+    }
 
-    // listed.delete()
-    const item1 = LinkedList.createNode(5)
-    listed.insert(item1)
-    const item2 = LinkedList.createNode(3)
-    listed.insert(item2)
-    const item3 = LinkedList.createNode(10)
-    listed.insert(item3)
-
-    // listed.delete(item2)
-    console.log(listed.find(3))
-
-    console.log(listed)
+    console.log(listed.reverseList(listed.head))
   }
 
   React.useEffect(() => {
--- src/views/LinkedList/linkedList.ts
+++ src/views/LinkedList/linkedList.ts
@@ -64,4 +64,24 @@ export default class LinkedList {
     }
     this.length--;
   }
+
+  // 反转链表
+
+  reverseList(head: ListNode | null): ListNode {
+    if (head === null || head.next === null) {
+      return head!
+    }
+  
+    // 递归反转 子链表
+    let newReverseList = this.reverseList(head.next)
+    // 获取原来链表的第2个节点newReverseListTail
+    let newReverseListTail = head.next
+    // 调整原来头结点和第2个节点的指向
+    newReverseListTail.next = head
+    head.next = null
+  
+    // 将调整后的链表返回
+    return newReverseList
+  }
 }
+
